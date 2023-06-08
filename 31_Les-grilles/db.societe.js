9aca9d99388f9b3a9d4a60b65284ac8e8a11e25e
DjibySec4 <djibysec4@gmail.com>
2023-06-08 12:26:41 +0000

    first commit

---
+++ 31_Les-grilles/db.societe.js
@@ -0,0 +1,20 @@
+db.societe.find()
+
+// db.societe.insertOne({
+//     nom: "Post Title 1",
+//     adresse: 
+//     {
+//         adresse1 : "Ouakam",
+//         adresse2 : "Mermoz"
+//     },
+//     email : "daaraytechnologie@gmail.com",
+//     nbEmploye : 12,
+//     speciaite : "Génie logiciel",
+//     dateCreation: Date(),
+//     nomDG : 
+//     {
+//         nom : "Sarr",
+//         prenom : "Modou", 
+//         sexe :  "Masculin"
+//     }
+// })
